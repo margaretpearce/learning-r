7095a746c9d63f88d3a820fb92b57eaa2130bb88
Margaret Pearce <margaret.pearce@gmail.com>
2017-01-19 16:12:51 -0500

    faceting

--- ggplot2_tutorial.R
+++ ggplot2_tutorial.R
@@ -1,5 +1,7 @@
+# ggplot2 tutorial from R bloggers
+# https://www.r-bloggers.com/basic-introduction-to-ggplot2/
+
 library(ggplot2)
-setInternet2(use=FALSE)
 
 myData <- read.table("data/ggplot2_tutorial_dataset.txt", header=T, sep="\t")
 str(myData)
@@ -15,3 +17,12 @@ qplot(data=myData, x=log(BM), y=log(var1), color=Tribe)
 
 # Boxplots
 qplot(data=myData, x=Hab, y=var1, geom="boxplot")
+
+# Faceting
+qplot(data=myData, x=BM, y=var1, log="xy", color=Tribe, facets=Hab~Tribe)
+qplot(data=myData, x=BM, y=var1, log="xy", color=Tribe, facets =~Tribe)
+
+# Add trend lines
+myplot <- qplot(data=myData, x=BM, y=var1, log="xy", color=Tribe, facets =~Tribe)
+myplot <- myplot + stat_smooth(method="lm")
+myplot

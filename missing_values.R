4a2ad700ec876229dbfbe8161dce94b5d46869ac
Margaret Pearce <margaret.pearce@gmail.com>
2017-01-22 19:59:01 -0500

    Outlier detection with Amelia

--- missing_values.R
+++ missing_values.R
@@ -26,3 +26,33 @@ mice_plot <- aggr(iris.mis, col=c("navyblue", "yellow"),
 # Impute the missing values (predictive mean matching)
 imputed_data <- mice(iris.mis, m=5, maxit=50, method="pmm", seed=500)
 summary(imputed_data)
+
+# Check imputed values
+imputed_data$imp$Sepal.Width
+
+# Get one of the five imputed data sets
+completeData <- complete(imputed_data,2)
+
+# Build predictive model with all five data sets
+fit <- with(data=completeData, exp=lm(Sepal.Width ~ Sepal.Length + Petal.Width))
+
+
+# Trying with another library
+library(Amelia)
+
+iris.mis <- prodNA(iris, noNA=0.1)
+summary(iris.mis)
+
+# Specify columns and run Amelia
+amelia_fit <- amelia(iris.mis, m=5, parallel="multicore", noms="Species")
+
+# Access imputed outputs
+amelia_fit$imputations[[1]]
+
+# Check a particular column in the data set
+amelia_fit$imputations[[5]]$Sepal.Length
+
+summary(amelia_fit$imputations[[5]])
+
+# Export the output to csv files
+write.amelia(amelia_fit, file.stem="imputed_data_set")

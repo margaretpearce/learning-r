30770679c24f2874bb897e79c1a7340f81130bfc
Margaret Pearce <margaret.pearce@gmail.com>
2017-01-27 23:12:16 -0500

    Plotting decision trees

--- rpart.R
+++ rpart.R
@@ -19,3 +19,16 @@ summary(data.rpart1)
 data.tree1 = tree(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data=data)
 summary(data.tree1)
 cv.tree(data.tree1)
+
+# Grow tree and specify classification
+fit <- rpart(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, 
+             method="class", 
+             data=data)
+
+printcp(fit) # display results
+plotcp(fit) # visualize cross-val results
+summary(fit) # detailed summary of splits
+
+# Plot the tree
+plot(fit, uniform = TRUE, main="Classification Tree for Iris")
+text(fit, use.n = TRUE, all = TRUE, cex = 0.8)

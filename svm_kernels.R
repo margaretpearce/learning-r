c8be6747f20c5ce3c6fabb3ee344eb47f39b9f5a
Margaret Pearce <margaret.pearce@gmail.com>
2017-01-30 21:20:31 -0500

    SVM tuning

--- svm_kernels.R
+++ svm_kernels.R
@@ -1,3 +1,6 @@
+# SVM tuning examples
+# Courtesy of: 
+# http://www.cs.upc.edu/~belanche/Docencia/mineria/English-september-2008/Practical-work/Labo-SVMs.R
 library(e1071)
 
 # Generate some artificial data
@@ -23,3 +26,27 @@ table(prediction_radial, classes)
 radial_svm_2 <- svm(data, classes, type='C', kernel='radial', gamma=0.1, cost=10)
 prediction_radial_2 <- predict(radial_svm_2, data)
 table(prediction_radial_2, classes)
+
+# Example:
+# Predict the type of glass based on its chemical analysis
+library(mlbench)
+library(rpart)
+
+data(Glass)
+
+# Split train/ test
+index <- 1:nrow(Glass)
+testindex <- sample(index, trunc(length(index)/3))
+testset <- Glass[testindex,]
+trainset <- Glass[-testindex,]
+
+# Fit the model
+glass_svm <- svm(Type ~ ., data=trainset, cost=100, gamma=1)
+glass_predictions <- predict(glass_svm, testset[,-10])
+table(pred=glass_predictions, true=testset[,10])
+
+# Tune using grid search
+glass_svm_tuned <- tune.svm(Type ~ ., data=trainset, gamma=2^(-1:1), cost = 2^(2:4))
+summary(glass_svm_tuned)
+
+plot(glass_svm_tuned, transform.x=log10, xlab=expression(log[10](gamma)), ylab="C")
